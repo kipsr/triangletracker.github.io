b2c1f44871e2dd5da70323811c61be0b2104ee6a
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-17 10:53:36 +0300

    add varibles

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-function submit(){
-    var a=parseInt(document.getElementById("a").value);
-    var b=parseInt(document.getElementById("b").value);
-    var c=parseInt(document.getElementById("c").value);
+function lengths(){
+    var a=document.getElementById("a").value;
+    var b=document.getElementById("b").value;
+    var c=document.getElementById("c").value;
+
+    var a=parseInt(a);
+    var b=parseInt(b);
+    var c=parseInt(c);
 
 if (( a+b <= c) || (b+c <= a) || (a+c <= b)){
  alert("Cannot form a triangle.");
 }
 else if((a===b) && (b===c)){
- alert("equilateral");
+ alert("Equilateral");
 }
 else if ((a===b) || (a===c) || (b===c)) {
- alert("isoceles");
+ alert("Isoceles");
 }
 else {
- alert("scalene");
+ alert("Scalene");
 }
 }
