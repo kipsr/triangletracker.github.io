c6bec7b4ada5c6d7df4d6041702baf30a599f99c
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-17 08:13:21 +0300

    add command lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function submit (){
-    var lenghth1= parseInt(document.getElementById(lenghth1))
+var a = parseInt(prompt("Length 1."));
+var b = parseInt(prompt("Length 2."));
+var c = parseInt(prompt("Length 3."));
+
+if (( a+b <= c) || (b+c <= a) || (a+c <= b)){
+ alert("Cannot form a triangle.");
+}
+else if((a===b) && (b===c)){
+ alert("equilateral");
+}
+else if ((a===b) || (a===c) || (b===c)) {
+ alert("isoceles");
+}
+else {
+ alert("scalene");
 }
